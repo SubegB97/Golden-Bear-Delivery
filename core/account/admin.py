e80d76b9c8aadaaa63c5d342f6a5c37625a683d2
SubegB97 <89858211+SubegB97@users.noreply.github.com>
2022-02-09 11:29:05 -0500

    First Committ

--- core/account/admin.py
+++ core/account/admin.py
@@ -7,4 +7,5 @@ class UserBaseAdmin(admin.ModelAdmin):
     list_display = ("first_name", "email", "user_name", "is_active", "is_driver")
 
 #registers the database model.
+# 
 admin.site.register(UserBase, UserBaseAdmin)
